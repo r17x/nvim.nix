4fa12f27dd9f03a5ba011d956b4d68abcfaea511
r17x <ri7nz@evilfactory.id>
2024-02-09 12:09:26 +0700

    chore: removed chatgpt from config/lsp.nix

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }:
 {
-  extraPlugins = with pkgs.vimPlugins; [
-    ChatGPT-nvim
-  ];
-
   extraPackages = [
     pkgs.ocamlPackages.ocaml-lsp
     # For better format in OCaml Lang need to install 
